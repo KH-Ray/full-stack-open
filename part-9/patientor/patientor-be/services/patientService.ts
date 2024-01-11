13360c05a4b2212029f0ca00d9eacf6fa4221506
K. Arrayyan <khanza_arrayyan@yahoo.co.id>
2024-01-11 09:05:29 +0700

    Finish exercise 9.8-9.13

---
+++ part-9/patientor/patientor-be/services/patientService.ts
@@ -0,0 +1,37 @@
+import patient from "../data/patients";
+import { Patient, NoSSNPatient, NewPatient } from "../types";
+import { v1 as uuid } from "uuid";
+
+const id = uuid();
+
+const patientDatas: Patient[] = patient;
+
+const getPatient = (): Patient[] => {
+  return patient;
+};
+
+const getNoSSNPatient = (): NoSSNPatient[] => {
+  return patient.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id,
+    ...patient,
+  };
+
+  patientDatas.push(newPatient);
+  return newPatient;
+};
+
+export default {
+  getPatient,
+  getNoSSNPatient,
+  addPatient,
+};
